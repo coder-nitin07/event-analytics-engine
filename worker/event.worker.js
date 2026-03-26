909a7e1301ba997e13f1813c10d1a682e4802160
coder-nitin07 <nitinthakur87504@gmail.com>
2026-03-26 22:56:13 +0530

    update worker

--- worker/event.worker.js
+++ worker/event.worker.js
@@ -1,23 +1,34 @@
 import { Worker } from "bullmq";
 import redisConnection from "../config/redis.config.js";
 
+// funtion to simulate delay
+const delay = (ms) => new Promise((resolve)=> setTimeout(resolve, ms));
+
 const worker = new Worker(
     "eventQueue",
     async (job)=>{
+        console.log(`Worker started processing Job`, job.id);
         console.log(`Processing Job`, job.data);
 
+        // heavy processing
+        await delay(2000);
 
-        return true;
+        console.log(`Processing event`, job.data.eventType);
+
+        // success result
+        return { stauts: "processed" };
     },
     {
         connection: redisConnection
     }
 );
 
-worker.on('completed', (job)=>{
-    console.log(`Job completed ${ job.id }`);
+worker.on('completed', (job, result)=>{
+    console.log(`Job completed ${ job.id }`, result);
 });
 
 worker.on('failed', (job, err)=>{
-    console.log(`Job failed: ${ job.id }`, err);
+    console.log(`Job failed: ${ job.id }`, err.message);
 });
+
+console.log("Worker is running...");
